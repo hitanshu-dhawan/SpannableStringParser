cbf3265fc935c3c948f7e96b75063e7c47417b85
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2019-12-22 22:33:02 +0530

    testColor and testTextDecorationLine

--- library/src/androidTest/java/com/hitanshudhawan/spannablestringparser/spanner/SpannerTest.kt
+++ library/src/androidTest/java/com/hitanshudhawan/spannablestringparser/spanner/SpannerTest.kt
@@ -1,21 +1,117 @@
 package com.hitanshudhawan.spannablestringparser.spanner
 
+import android.graphics.Color
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
+import android.text.style.StrikethroughSpan
+import android.text.style.UnderlineSpan
 import com.hitanshudhawan.spannablestringparser.spannify
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class SpannerTest {
 
     @Test
-    fun test001() {
-        val a = "{ `Hitanshu` < color : #0000FF /> }"
-        val b = a.spannify()
-        val c = (b as SpannableStringBuilder).getSpans(0, b.length, Any::class.java)
-        assertEquals(
-            "#0000FF",
-            String.format("#%06X", (0xFFFFFF and (c[0] as ForegroundColorSpan).foregroundColor))
-        )
+    fun testColor001() {
+        val string = "{ `Hitanshu` < color : #0000FF /> }"
+        with(string.spannify()) {
+            val spannable = this as SpannableStringBuilder
+
+            val spans1 = spannable.getSpans()
+            assertTrue(spans1.size == 1)
+            assertEquals(Color.parseColor("#0000FF"), (spans1[0] as ForegroundColorSpan).foregroundColor)
+        }
     }
+
+    @Test
+    fun testColor002() {
+        val string = "{ `Hitanshu` < color : #AA0000FF /> }"
+        with(string.spannify()) {
+            val spannable = this as SpannableStringBuilder
+
+            val spans1 = spannable.getSpans()
+            assertTrue(spans1.size == 1)
+            assertEquals(Color.parseColor("#AA0000FF"), (spans1[0] as ForegroundColorSpan).foregroundColor)
+        }
+    }
+
+    @Test
+    fun testColor003() {
+        val string = "Hello { `Hitanshu` < color : #0000FF /> } Dhawan"
+        with(string.spannify()) {
+            val spannable = this as SpannableStringBuilder
+
+            val spans1 = spannable.getSpans(queryStart = 0, queryEnd = 6)
+            assertTrue(spans1.isEmpty())
+            val spans2 = spannable.getSpans(queryStart = 6, queryEnd = 14)
+            assertTrue(spans2.size == 1)
+            assertEquals(Color.parseColor("#0000FF"), (spans2[0] as ForegroundColorSpan).foregroundColor)
+            val spans3 = spannable.getSpans(queryStart = 14, queryEnd = length)
+            assertTrue(spans3.isEmpty())
+        }
+    }
+
+    @Test
+    fun testTextDecorationLine001() {
+        val string = "{ `Hitanshu` < text-decoration-line : underline /> }"
+        with(string.spannify()) {
+            val spannable = this as SpannableStringBuilder
+
+            val spans1 = spannable.getSpans()
+            assertTrue(spans1.size == 1)
+            assertTrue(spans1[0] is UnderlineSpan)
+        }
+    }
+
+    @Test
+    fun testTextDecorationLine002() {
+        val string = "{ `Hitanshu` < text-decoration-line : line-through /> }"
+        with(string.spannify()) {
+            val spannable = this as SpannableStringBuilder
+
+            val spans1 = spannable.getSpans()
+            assertTrue(spans1.size == 1)
+            assertTrue(spans1[0] is StrikethroughSpan)
+        }
+    }
+
+    @Test
+    fun testTextDecorationLine003() {
+        val string = "Hello { `Hitanshu` < text-decoration-line : underline /> } Dhawan"
+        with(string.spannify()) {
+            val spannable = this as SpannableStringBuilder
+
+            val spans1 = spannable.getSpans(queryStart = 0, queryEnd = 6)
+            assertTrue(spans1.isEmpty())
+            val spans2 = spannable.getSpans(queryStart = 6, queryEnd = 14)
+            assertTrue(spans2.size == 1)
+            assertTrue(spans2[0] is UnderlineSpan)
+            val spans3 = spannable.getSpans(queryStart = 14, queryEnd = length)
+            assertTrue(spans3.isEmpty())
+        }
+    }
+
+    @Test
+    fun testTextDecorationLine004() {
+        val string = "Hello { `Hitanshu` < text-decoration-line : line-through /> } Dhawan"
+        with(string.spannify()) {
+            val spannable = this as SpannableStringBuilder
+
+            val spans1 = spannable.getSpans(queryStart = 0, queryEnd = 6)
+            assertTrue(spans1.isEmpty())
+            val spans2 = spannable.getSpans(queryStart = 6, queryEnd = 14)
+            assertTrue(spans2.size == 1)
+            assertTrue(spans2[0] is StrikethroughSpan)
+            val spans3 = spannable.getSpans(queryStart = 14, queryEnd = length)
+            assertTrue(spans3.isEmpty())
+        }
+    }
+
+    //...
+
+    private fun SpannableStringBuilder.getSpans(queryStart: Int = 0, queryEnd: Int = length): Array<Any> {
+        return getSpans(queryStart, queryEnd, Any::class.java)
+    }
+
 }
