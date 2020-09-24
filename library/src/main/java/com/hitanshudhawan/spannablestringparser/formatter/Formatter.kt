1153765000536ad953b30edc67747332c98fcf33
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-09-24 12:18:51 +0530

    Logic for coverting to String

--- library/src/main/java/com/hitanshudhawan/spannablestringparser/formatter/Formatter.kt
+++ library/src/main/java/com/hitanshudhawan/spannablestringparser/formatter/Formatter.kt
@@ -1,6 +1,9 @@
 package com.hitanshudhawan.spannablestringparser.formatter
 
 import com.hitanshudhawan.spannablestringparser.lexer.TokenType
+import com.hitanshudhawan.spannablestringparser.parse
+import com.hitanshudhawan.spannablestringparser.parser.Declaration
+import com.hitanshudhawan.spannablestringparser.parser.Node
 import com.hitanshudhawan.spannablestringparser.tokenize
 
 internal class Formatter(private val text: String) {
@@ -20,6 +23,40 @@ internal class Formatter(private val text: String) {
         if (values.any { it.isBlank() })
             throw IllegalArgumentException("...")
 
-        return "..."
+        val syntaxTree = text.tokenize().parse()
+
+        if (syntaxTree.size != 1)
+            throw IllegalArgumentException("...")
+
+        val node = syntaxTree[0].copy(
+            declarations = (syntaxTree[0].declarations ?: emptyList()).toMutableList().apply {
+                addAll(values.map { value -> Declaration(property, value) })
+            }
+        )
+
+        return node.covertToString()
+    }
+
+    private fun Node.covertToString(): String {
+        if (this.declarations == null || this.declarations.isEmpty())
+            return this.text
+
+        val template = "{ `{{TEXT}}` <{{DECLARATIONS}}/> }"
+
+        return template
+            .replace("{{TEXT}}", this.text)
+            .replace(
+                "{{DECLARATIONS}}",
+                this.declarations
+                    .groupBy { it.property }
+                    .map { (property, declarations) ->
+                        "$property:${declarations.map { it.value.wrapWithBackticksIfInvalid() }.joinToString("|")}"
+                    }
+                    .joinToString(";")
+            )
+    }
+
+    private fun String.wrapWithBackticksIfInvalid(): String {
+        return if (this.tokenize().size != 1) "`$this`" else this
     }
 }
